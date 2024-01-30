854c53894bec85692478e483c745e59d289d59b1
Negar Kakaei <92810954+negarkakaei@users.noreply.github.com>
2024-01-30 23:38:38 +0330

    test edited

--- bakend/test_app/test_unittest.py
+++ bakend/test_app/test_unittest.py
@@ -6,84 +6,85 @@ import unittest
 
 class Record_controller_test(unittest.TestCase):
 
-    def should_raiseValidationError_when_recordDoesntExist():
+    def should_raiseValidationError_when_recordDoesntExist(self):
         rid = 'invalidID'
         result = delete_record(rid)
-        assert result == 'invalid record_id'
+        self.assertEqual(result, 'invalid recored_id')
         
 
-    def should_returnAddedRecord_when_addRecordSuccessfull():
+    def should_returnAddedRecord_when_addRecordSuccessfull(self):
         record = Record(amount=20, category='food', date='1/30/2024', title='record1', user_id='realID')
         result = add_record(record, 'realID')
-        assert result == record
+        self.assertEqual(result, record)
 
-    def should_raiseDuplicationError_when_recordNameIsReplicate():
+    def should_raiseDuplicationError_when_recordNameIsReplicate(self):
         record = Record(amount=20, category='food', date='1/30/2024', title='record1', user_id='realID')
         result = add_record(record, 'realID')
-        assert result == 'duplicated record name'
+        self.assertEqual(result, 'duplicated record name')
 
-    def should_raiseValidationError_when_categoryNotProvided():
+    def should_raiseValidationError_when_categoryNotProvided(self):
         record = Record(amount=20, date='1/30/2024', title='record1', user_id='realID', category= None)
         result = add_record(record, 'realID')
-        assert result == 'invalid category_id'
+        self.assertEqual(result, 'invalid category_id')
 
-    def should_raiseValidationError_when_categoryDoesntExist():
+    def should_raiseValidationError_when_categoryDoesntExist(self):
         record = Record(amount=20, category='random' ,date='1/30/2024', title='record1', user_id='realID')
         result = add_record(record, 'realID')
-        assert result == 'invalid category_id'
+        self.assertEqual(result, 'invalid category_id')
 
 
 
 class database_connectivity_test(unittest.TestCase):
 
-    def should_returnRecord_when_recordSuccessfullyAdded():
+    def should_returnRecord_when_recordSuccessfullyAdded(self):
         amount = 20 
         uid = 'realID'
         date = '1/30/2024'
         record = Record(amount = amount, date=date, user_id = uid, category = None, title =None)
         result = create_record(record)
-        assert result == record
+        self.assertEqual(result, record)
 
-    def should_failAddingRecord_when_dateNotProvided():
+    def should_failAddingRecord_when_dateNotProvided(self):
         amount = 20 
         uid = 'realID'
         record = Record(amount = amount, user_id = uid, title = None, category = None, date = None)
         result = create_record(record)
-        assert result != record
+        self.assertNotEqual(result, record)
 
-    def should_failAddingRecord_when_amountNotProvided():
+    def should_failAddingRecord_when_amountNotProvided(self):
         uid = 'realID'
         date = '1/30/2024'
         record = Record(date = date, user_id = uid, title = None, category = None, amount = None)
         result = create_record(record)
-        assert result != record
+        self.assertNotEqual(result, record)
 
-    def should_failAddingRecord_when_uidNotProvided():
+    def should_failAddingRecord_when_uidNotProvided(self):
         date = '1/30/2024'
         record = Record(amount = amount, date = date, title = None, category = None, user_id = None)
         result = create_record(record)
-        assert result != record
+        self.assertNotEqual(result, record)
 
 
-    def should_returnTrue_when_recordExists():
+    def should_returnTrue_when_recordExists(self):
         result = record_exist_id('realID')
-        assert result == True
+        self.assertEqual(result, True)
 
-    def should_returnFalse_when_recordDoesntExist():
+    def should_returnFalse_when_recordDoesntExist(self):
         result = record_exist_id('123456')
-        assert result == False
+        self.assertEqual(result, False)
 
-    def should_returnTrue_when_categoryExistsAndValidID():
+    def should_returnTrue_when_categoryExistsAndValidID(self):
         result = category_exist('food','realID')
-        assert result == True
+        self.assertEqual(result, True)
 
-    def should_returnFalse_when_categoryDoesntExist():
+
+    def should_returnFalse_when_categoryDoesntExist(self):
         result = category_exist('random','realID')
-        assert result == False
+        self.assertEqual(result, False)
 
-    def should_returnFalse_when_invalidUserID():
+    def should_returnFalse_when_invalidUserID(self):
         result = category_exist('food', '123456')
-        assert result == False
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':
