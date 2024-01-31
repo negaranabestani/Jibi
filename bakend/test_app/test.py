a216d80b3f8c6a117ea7a51e04965ab5344efe58
Negar Kakaei <92810954+negarkakaei@users.noreply.github.com>
2024-01-31 14:40:58 +0330

    bugs

--- bakend/test_app/test.py
+++ bakend/test_app/test.py
@@ -1,6 +1,4 @@
 import unittest
-
-from app.db.database_connectivity import create_record, category_exist, record_exist_id
 from app.controller.record_controller import remove_record, add_record, add_category
 from app.controller.user_controller import sign_up, generate_token
 from app.entity.category import Category
@@ -33,17 +31,28 @@ class User_controller_test(unittest.TestCase):
         def test_should_returnCategory_when_addedSuccessfully(self):
             # ToDo user id is required
             category = Category(color='red', icon='circle', title='food', user_id=1)
-            result = add_category(category)
+            result = add_category(category, 1)
             self.assertIsNotNone(result)
 
         def test_should_raiseDuplicationError_when_categoryNameIsReplicate(self):
             category = Category(color='red', icon='circle', title='food', user_id=1)
-            self.assertRaises(DuplicationException, add_category(category))
+            p = False
+            try:
+                add_category(category, 1)
+            except DuplicationException:
+                p = True
+            self.assertTrue(p)
 
         # remove record
         def test_should_raiseValidationError_when_recordDoesntExist(self):
             rid = 0
-            self.assertRaises(ValidationException, remove_record(rid))
+            p = False
+            try:
+                remove_record(rid)
+            except ValidationException:
+                p = True
+            self.assertTrue(p)
+
 
         # add record
         def test_should_returnAddedRecord_when_addRecordSuccessfull(self):
@@ -53,15 +62,31 @@ class User_controller_test(unittest.TestCase):
 
         def test_should_raiseDuplicationError_when_recordNameIsReplicate(self):
             record = Record(amount=20, category=1, date='1/30/2024', title='record1', user_id=1)
-            self.assertRaises(DuplicationException, add_record(record, 1))
+            p = False
+            try:
+                add_record(record, 1)
+            except DuplicationException:
+                p = True
+            self.assertTrue(p)
 
         def test_should_raiseValidationError_when_categoryNotProvided(self):
             record = Record(amount=20, date='1/30/2024', title='record1', user_id=1, category=None)
-            self.assertRaises(ValidationException, add_record(record, 1))
+            p = False
+            try:
+                add_record(record, 1)
+            except ValidationException:
+                p = True
+            self.assertTrue(p)
 
         def test_should_raiseValidationError_when_categoryDoesntExist(self):
             record = Record(amount=20, category=0, date='1/30/2024', title='record1', user_id=1)
-            self.assertRaises(ValidationException, add_record(record, 1))
+            p = False
+            try:
+                add_record(record, 1)
+            except ValidationException:
+                p = True
+            self.assertTrue(p)
+
 
     if __name__ == '__main__':
         unittest.main()
